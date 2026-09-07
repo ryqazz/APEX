c09b0e411c82b803d46412dc627b1b41d9d96004
Daniel Elauria De Dios <daniel.dedios099@gmail.com>
2026-09-07 20:02:25 +0800

    Delete APEX directory

--- APEX/Apex Gateway/config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-OLLAMA_TARGET_URL = "http://192.168.0.20:11434/api/generate"
-ONNX_MODEL_PATH = "deberta_v3_small_prompt_injection.onnx"
-TOKENIZER_NAME = "microsoft/deberta-v3-small"
-ML_CONFIDENCE_THRESHOLD = 0.5
